3872621dda7335af44f45acaf95335354ac19590
Matt Shanahan <mrshanahan11235@gmail.com>
2024-01-01 15:02:33 -0600

    Add janky manual integration test file

---
+++ cmd/notes-test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+    "fmt"
+
+    "mrshanahan.com/notes-api/pkg/client"
+    "mrshanahan.com/notes-api/pkg/notes"
+)
+
+func main() {
+    client := client.NewClient("http://localhost:3333/")
+    // note, err := client.GetNote(40)
+    // if err != nil {
+    //     panic(err)
+    // }
+    // printNote(note)
+    notes, err := client.ListNotes()
+    if err != nil {
+        panic(err)
+    }
+    for _, n := range notes {
+        printNote(n)
+    }
+
+    fmt.Println("--------------")
+
+    note, err := client.CreateNote("this is a new note")
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("New note:")
+    printNote(note)
+
+    fmt.Println("--------------")
+
+    notes, err = client.ListNotes()
+    if err != nil {
+        panic(err)
+    }
+    for _, n := range notes {
+        printNote(n)
+    }
+
+    fmt.Println("--------------")
+
+    note, err = client.GetNote(note.ID)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("GOT new note:")
+    printNote(note)
+
+    fmt.Println("--------------")
+
+    err = client.UpdateNoteContent(note.ID, []byte("This is the\nnew content!"))
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Content updated!")
+
+    fmt.Println("--------------")
+
+    content, err := client.GetNoteContent(note.ID)
+    fmt.Println("Content for new note:")
+    fmt.Println(string(content))
+
+    fmt.Println("--------------")
+
+    err = client.DeleteNote(note.ID)
+    if err != nil {
+        panic(err)
+    }
+    fmt.Println("Deleted!")
+
+    fmt.Println("--------------")
+
+    notes, err = client.ListNotes()
+    if err != nil {
+        panic(err)
+    }
+    for _, n := range notes {
+        printNote(n)
+    }
+}
+
+func printNote(n *notes.Note) {
+    fmt.Printf("[%d] %s (created: %s, updated: %s)\n", n.ID, n.Title, n.CreatedOn, n.UpdatedOn)
+}
